8fb3515cd8bac3c82aea7071f0c3ba4e397a5d9f
Antonio Ordóñez <aom387@inlumine.ual.es>
2024-11-16 12:49:25 +0100

    Subida listado tarjetas y menú.

---
+++ pokedex/pokedex/VistaAjustes.swift
@@ -0,0 +1,18 @@
+//
+//  VistaAjustes.swift
+//  pokedex
+//
+//  Created by Antonio Ordóñez on 16/11/24.
+//
+
+import SwiftUI
+
+struct VistaAjustes: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VistaAjustes()
+}
