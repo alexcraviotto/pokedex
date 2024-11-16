7f86a115793e354fd94367c81c6d67eb6e762987
Antonio Ordóñez <aom387@inlumine.ual.es>
2024-11-16 19:06:01 +0100

    Navegabilidad incial

--- pokedex/pokedex/VistaRegistrarse.swift
+++ pokedex/pokedex/VistaRegistrarse.swift
@@ -12,55 +12,62 @@ struct VistaRegistrarse: View {
     @Binding var contrasena: String
     @Binding var repetirContrasena: String
     @State private var mensajeError: String = ""
+    @Environment(\.presentationMode) var presentationMode
     private var camposRellenos: Bool {
            !nombreUsuario.isEmpty && !contrasena.isEmpty && !repetirContrasena.isEmpty
        }
     var body: some View {
-        ZStack {
-            Color(red: 40 / 255, green: 47 / 255, blue: 56 / 255)
-                .edgesIgnoringSafeArea(.all)
-            VStack {
-                Image("logoRegistrarse").padding(.bottom)
-                
-                TextField("Nombre de usuario", text: $nombreUsuario)
-                                .padding(5)
-                                .frame(width: 350, height: 50)
-                                .overlay(Rectangle().stroke(Color.gray, lineWidth: 2))
-                                .disableAutocorrection(true)
-                                .scrollContentBackground(.hidden)
-                                .background(Color.white).cornerRadius(10)
-                TextField("Contraseña", text: $contrasena)
-                                .padding(5)
-                                .frame(width: 350, height: 50)
-                                .overlay(Rectangle().stroke(Color.gray, lineWidth: 2))
-                                .disableAutocorrection(true)
-                                .scrollContentBackground(.hidden)
-                                .background(Color.white)
-                                .cornerRadius(10)
-                TextField("Repita su contraseña", text: $repetirContrasena)
-                                .padding(5)
-                                .frame(width: 350, height: 50)
-                                .overlay(Rectangle().stroke(Color.gray, lineWidth: 2))
-                                .disableAutocorrection(true)
-                                .scrollContentBackground(.hidden)
-                                .background(Color.white)
-                                .cornerRadius(10).disabled(!camposRellenos)
-          
-                
-                Image("imagenRegistro").resizable().scaledToFit().padding()
-                
-                Button("Registrarse") {
-                    guard contrasena == repetirContrasena else {
-                    mensajeError = "Las contraseñas no coinciden. Por favor, inténtalo de nuevo."
-                    return
-                    }
+        NavigationView {
+            ZStack {
+                Color(red: 40 / 255, green: 47 / 255, blue: 56 / 255)
+                    .edgesIgnoringSafeArea(.all)
+                VStack {
+                    Image("logoRegistrarse").padding(.bottom)
                     
-                }.frame(width: 350, height: 50)
-                    .overlay(Rectangle().stroke(Color.black, lineWidth: 2))
-                .background(Color(red: 100 / 255, green: 80 / 255, blue: 0 / 255)).foregroundColor(.black).padding(10).cornerRadius(10)
+                    TextField("Nombre de usuario", text: $nombreUsuario)
+                        .padding(5)
+                        .frame(width: 350, height: 50)
+                        .overlay(Rectangle().stroke(Color.gray, lineWidth: 2))
+                        .disableAutocorrection(true)
+                        .scrollContentBackground(.hidden)
+                        .background(Color.white).cornerRadius(10)
+                    TextField("Contraseña", text: $contrasena)
+                        .padding(5)
+                        .frame(width: 350, height: 50)
+                        .overlay(Rectangle().stroke(Color.gray, lineWidth: 2))
+                        .disableAutocorrection(true)
+                        .scrollContentBackground(.hidden)
+                        .background(Color.white)
+                        .cornerRadius(10)
+                    TextField("Repita su contraseña", text: $repetirContrasena)
+                        .padding(5)
+                        .frame(width: 350, height: 50)
+                        .overlay(Rectangle().stroke(Color.gray, lineWidth: 2))
+                        .disableAutocorrection(true)
+                        .scrollContentBackground(.hidden)
+                        .background(Color.white)
+                        .cornerRadius(10).disabled(!camposRellenos)
+                    
+                    
+                    Image("imagenRegistro").resizable().scaledToFit().padding()
+                    
+                    Button("Registrarse") {
+                        guard contrasena == repetirContrasena else {
+                            mensajeError = "Las contraseñas no coinciden. Por favor, inténtalo de nuevo."
+                            return
+                        }
+                        
+                    }.frame(width: 350, height: 50)
+                        .overlay(Rectangle().stroke(Color.black, lineWidth: 2))
+                        .background(Color(red: 100 / 255, green: 80 / 255, blue: 0 / 255)).foregroundColor(.black).padding(10).cornerRadius(10)
+                }
             }
-           
-            }
+        }.navigationBarBackButtonHidden(true).navigationBarItems(leading: Button(action: {
+            presentationMode.wrappedValue.dismiss()
+        }) {
+            Text("< Iniciar sesión")
+                .foregroundColor(.yellow)
+        })
         }
     }
 
