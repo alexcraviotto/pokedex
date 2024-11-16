11ccb4a34667b546a55dc29c70db87654912cbfb
Antonio Ordóñez <aom387@inlumine.ual.es>
2024-11-16 17:39:02 +0100

    Cambios iniciar sesión y registrarse.

---
+++ pokedex/pokedex/VistaRegistrarse.swift
@@ -0,0 +1,18 @@
+//
+//  VistaRegistrarse.swift
+//  pokedex
+//
+//  Created by Antonio Ordóñez on 16/11/24.
+//
+
+import SwiftUI
+
+struct VistaRegistrarse: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VistaRegistrarse()
+}
