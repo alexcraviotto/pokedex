e4e3ec8ad9629b16b8ba8d84ed6309643ed1723c
aer234 <113684767+aer234@users.noreply.github.com>
2024-11-19 17:47:02 +0100

    Add files via upload

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Aula03 on 12/11/24.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Inicio()
+        }
+    }
+}
