14496a1b5c556abd0f53d6a2f6fea1c2a01a431d
alexcraviotto <agc710@inlumine.ual.es>
2024-12-03 16:52:25 +0100

    conexion de busqueda

--- pokedex/VistaBusqueda.swift
+++ pokedex/VistaBusqueda.swift
@@ -17,90 +17,67 @@ struct VistaBusqueda: View {
     ]
     
     @State var query: String = ""
-    //@State var aux:[Pokemon] = []
-    
     @State var filtrado: [Pokemon] = []
-    /*{
-        var names:[String] = []
-        names = filterPokemonByName(pokemonArray: pokemons, searchTerm: query)
-        if(names.count == aux.count){
-            return aux
-        }
-        print(aux.count)
-        print("names \(names.count)")
-        if query.count >= 3 {
-            for nombre in names {
-                fetchPokemonData(pokemonId: nombre) { result in
-                    switch result {
-                    case .success(let newpokemon):
-                        aux.append(newpokemon)
-                        //print("tamanno \(aux.count)")
-                    case .failure(let error):
-                        print("Error: \(error)")
-
-                    }
-                }
-            }
-            //print("tamanno al final  \(aux.count)")
-            return aux
-        }
-        return []
-    }*/
 
     var body: some View {
-        VStack {
-            HStack{
-                Text("Búsqueda")
-                    .font(.custom("Press Start 2P Regular", size: 24))
-                    .foregroundColor(.black)
-                Spacer()
-            }.padding()
-            BusquedaView(text:$query)
-                .onChange(of: query) { oldValue, newValue in
-                    print(pokemons)
-                    filtrado = []
-                    if query.count >= 3 {
-                        for nombre in filterPokemonByName(pokemonArray: pokemons, searchTerm: query) {
-                            print(nombre)
-                            fetchPokemonData(pokemonId: nombre) { result in
-                                switch result {
-                                case .success(let newpokemon):
-                                    filtrado.append(newpokemon)
-                                    //print("tamanno \(aux.count)")
-                                case .failure(let error):
-                                    print("Error: \(error)")
-
+        NavigationView {
+            VStack {
+                HStack {
+                    Text("Búsqueda")
+                        .font(.custom("Press Start 2P Regular", size: 24))
+                        .foregroundColor(.black)
+                    Spacer()
+                }.padding()
+                
+                BusquedaView(text: $query)
+                    .onChange(of: query) { oldValue, newValue in
+                        print(pokemons)
+                        filtrado = []
+                        if query.count >= 3 {
+                            for nombre in filterPokemonByName(pokemonArray: pokemons, searchTerm: query) {
+                                print(nombre)
+                                fetchPokemonData(pokemonId: nombre) { result in
+                                    switch result {
+                                    case .success(let newpokemon):
+                                        filtrado.append(newpokemon)
+                                    case .failure(let error):
+                                        print("Error: \(error)")
+                                    }
                                 }
                             }
                         }
                     }
-                }
-            ScrollView {
-                if !filtrado.isEmpty {
-                    LazyVGrid(columns: columnas, spacing: 20) {
-                        ForEach(filtrado) { pokemon in
-                            PokemonTarjeta2(
-                                pokemon: pokemon
-                            )
-                            .scaleEffect(0.9)
+                
+                ScrollView {
+                    if !filtrado.isEmpty {
+                        LazyVGrid(columns: columnas, spacing: 20) {
+                            ForEach(filtrado) { pokemon in
+                                NavigationLink(
+                                    destination: NavegacionVistaDetalle(pokemon: pokemon)
+                                ) {
+                                    PokemonTarjeta2(
+                                        pokemon: pokemon
+                                    )
+                                    .scaleEffect(0.9)
+                                }
+                            }
                         }
+                    } else if query.count >= 3 {
+                        Text("No se encontraron resultados").padding()
                     }
-                } else if query.count >= 3 {
-                    Text("No se encontraron resultados").padding()
                 }
             }
-        }.onAppear(){
-            fetchPokemonNames { result in
-                switch result {
-                case .success(let pokemons):
-                    self.pokemons.append(contentsOf: pokemons)
-                case .failure(let error):
-                    print("Error: \(error)")
+            .onAppear {
+                fetchPokemonNames { result in
+                    switch result {
+                    case .success(let pokemons):
+                        self.pokemons.append(contentsOf: pokemons)
+                    case .failure(let error):
+                        print("Error: \(error)")
+                    }
                 }
             }
-            
         }
-            
     }
     
     func filterPokemonByName(pokemonArray: [PokemonPair], searchTerm: String) -> [String] {
@@ -111,27 +88,26 @@ struct VistaBusqueda: View {
     }
 }
 
-
 struct BusquedaView: View {
     @Binding var text: String
     var body: some View {
-        HStack{
+        HStack {
             Image(systemName: "magnifyingglass")
                 .foregroundColor(text.isEmpty
-                ? Color(UIColor.gray).opacity(0.6)
-                : Color(UIColor.gray).opacity(0.9))
-            TextField("Buscar…", text:$text)
-            Button(){
+                    ? Color(UIColor.gray).opacity(0.6)
+                    : Color(UIColor.gray).opacity(0.9))
+            TextField("Buscar…", text: $text)
+            Button {
                 text = ""
-            }label:{
+            } label: {
                 Image(systemName: "x.circle")
             }.opacity(text.isEmpty ? 0.0 : 1.0)
         }
-        .frame(width: 320, height: 30).shadow(radius: 9)
+        .frame(width: 320, height: 30)
+        .shadow(radius: 9)
         .background(
             RoundedRectangle(cornerRadius: 9)
-            .fill(Color.gray.opacity(0.1))
+                .fill(Color.gray.opacity(0.1))
         )
     }
-     
 }
